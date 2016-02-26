198c8fde114bb0170b83e43c161352aeae919e83
ebarrier <ebarrier@itcollege.ee>
2016-02-26 13:01:17 +0200

    Error checking db and subtotals

--- cart.php
+++ cart.php
@@ -2,8 +2,9 @@
 require_once "config.php";
 include "header.php";
 $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+$conn or die("Connection to database failed:".$conn->connect_error); /*this line is equivalent to 
 if ($conn->connect_error)
-  die("Connection to database failed:".$conn->connect_error);
+  die("Connection to database failed:".$conn->connect_error);*/
 $conn->query("set names utf8");
 
 $product_id = intval($_POST["id"]); //intval to make the value an integer
@@ -25,6 +26,7 @@ if (array_key_exists($product_id, $_SESSION["cart"])) {
 
 <?php 
 $result = $conn->query("SELECT id, Name, Price FROM etienne;");
+$result or die("Connection to database failed:".$conn->connect_error); //if result is true, the second expression is not checked.
 
 while ($row = $result->fetch_assoc()) {
   if (array_key_exists($row['id'], $_SESSION["cart"])) {
