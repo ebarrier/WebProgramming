7bc82354066a0b23ca88f550df90d1b3322c3666
ebarrier <ebarrier@itcollege.ee>
2016-02-26 13:17:16 +0200

    Button delete without function + Request method check

--- cart.php
+++ cart.php
@@ -7,13 +7,17 @@ if ($conn->connect_error)
   die("Connection to database failed:".$conn->connect_error);*/
 $conn->query("set names utf8");
 
-$product_id = intval($_POST["id"]); //intval to make the value an integer
-if (array_key_exists($product_id, $_SESSION["cart"])) {
-    $_SESSION["cart"][$product_id] += 1;
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  // We are adding something to the cart
+  $product_id = intval($_POST["id"]); //intval to make the value an integer
+  if (array_key_exists($product_id, $_SESSION["cart"])) {
+      $_SESSION["cart"][$product_id] += 1;
+  } else {
+      $_SESSION["cart"][$product_id] = 1;
+  }
 } else {
-    $_SESSION["cart"][$product_id] = 1;
+    // We are just checking out what we have in the cart
 }
-
 ?>
 
 <h1>Etienne's webshop</h1>
@@ -29,12 +33,17 @@ $result = $conn->query("SELECT id, Name, Price FROM etienne;");
 $result or die("Connection to database failed:".$conn->connect_error); //if result is true, the second expression is not checked.
 
 while ($row = $result->fetch_assoc()) {
+  $product_id= $row['id'];
   if (array_key_exists($row['id'], $_SESSION["cart"])) {
   $count = $_SESSION["cart"][$row['id']];?>   
   <li>
     <?=$count;?> item(s) of <a href="description.php?id=<?=$row['id']?>">
       <?=$row['Name']?></a>
       <?=$row['Price']?>Eur. <?= $row['Price']*$count?> in total
+    <form method="delete">
+      <input type="hidden" name="id" value="<?=$product_id;?>"/>
+      <input type="submit" value="Remove"/>
+    </form>
   </li>
 
   <?php
